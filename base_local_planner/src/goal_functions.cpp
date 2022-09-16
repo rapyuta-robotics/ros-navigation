9b5fa86264bf7fdf4ff8cb58485ee67a6b69774f
siferati <15384781+siferati@users.noreply.github.com>
2022-09-16 10:40:03 +0900

    remove not needed include

--- base_local_planner/src/goal_functions.cpp
+++ base_local_planner/src/goal_functions.cpp
@@ -38,7 +38,6 @@
 #include <tf2/LinearMath/Matrix3x3.h>
 #include <tf2/utils.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-#include <numeric>
 #ifdef _MSC_VER
 #define GOAL_ATTRIBUTE_UNUSED
 #else
