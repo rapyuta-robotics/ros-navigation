623952788a4567e44ff3ffb9d0e71eb57cee8f11
Renan Salles <renan028@gmail.com>
2023-11-08 14:30:25 +0900

    Add FixedOrientation (Omni) and Sideward orientation modes (#89)

--- global_planner/src/orientation_filter.cpp
+++ global_planner/src/orientation_filter.cpp
@@ -97,6 +97,26 @@ void OrientationFilter::processPath(const geometry_msgs::PoseStamped& start,
                 }
             }
             break;
+        case SIDEWARD:
+            for (int i = 0; i < n - 1; i++) {
+                setAngleBasedOnPositionDerivative(path, i);
+            }
+            if (n > 2) {
+                const int num_skips = n >= 5 ? 2 : 1;
+                const double start_to_path_theta =  min_angle(start, *std::next(path.begin(), num_skips));
+                const double path_to_goal_theta =  min_angle(*std::prev(path.end(), 1 + num_skips), path.back());
+
+                const bool prefer_leftward = 
+                    std::fabs(angles::normalize_angle(start_to_path_theta - M_PI_2)) + std::fabs(angles::normalize_angle(path_to_goal_theta + M_PI_2)) < 
+                    std::fabs(angles::normalize_angle(start_to_path_theta + M_PI_2)) + std::fabs(angles::normalize_angle(path_to_goal_theta - M_PI_2));
+
+                for (int i = 0; i < n - 1; i++) {
+                    const double path_orientation = tf2::getYaw(path[i].pose.orientation);
+                    const double adjusted_orientation = prefer_leftward ? path_orientation - M_PI_2 : path_orientation + M_PI_2;
+                    set_angle(&path[i], angles::normalize_angle(adjusted_orientation));
+                }
+            }
+            break;
         case LEFTWARD:
             for(int i=0;i<n-1;i++){
                 setAngleBasedOnPositionDerivative(path, i);
@@ -107,6 +127,11 @@ void OrientationFilter::processPath(const geometry_msgs::PoseStamped& start,
             for(int i=0;i<n-1;i++){
                 setAngleBasedOnPositionDerivative(path, i);
                 set_angle(&path[i], angles::normalize_angle(tf2::getYaw(path[i].pose.orientation) + M_PI_2));
+            }
+            break;
+        case FIXEDORIENTATION:
+            for(int i=0;i<n-1;i++){
+                path[i].pose.orientation = start.pose.orientation;
             }
             break;
         case INTERPOLATE:
@@ -161,6 +186,6 @@ void OrientationFilter::interpolate(std::vector<geometry_msgs::PoseStamped>& pat
         set_angle(&path[i], angle);
     }
 }
-                                   
+
 
 };
