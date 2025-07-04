68e2d82bded5c1f9f9eede8c8b3dca1f9682afc4
Renan Salles <renan028@gmail.com>
2025-07-04 13:54:56 +0900

    Revert "Update padFootprint function (#108)"
    This reverts commit e434246f2b2f365fd1e591e68a016e6f514e8422.

--- costmap_2d/test/footprint_tests.cpp
+++ costmap_2d/test/footprint_tests.cpp
@@ -70,17 +70,17 @@ TEST( Costmap2DROS, padded_footprint_from_string_param )
   std::vector<geometry_msgs::Point> footprint = cm.getRobotFootprint();
   EXPECT_EQ( 3, footprint.size() );
 
-  EXPECT_NEAR( 1.44721f, footprint[ 0 ].x, 0.0001f);
-  EXPECT_NEAR( 1.22361f, footprint[ 0 ].y, 0.0001f);
-  EXPECT_EQ( 0.0f, footprint[ 0 ].z);
+  EXPECT_EQ( 1.5f, footprint[ 0 ].x );
+  EXPECT_EQ( 1.5f, footprint[ 0 ].y );
+  EXPECT_EQ( 0.0f, footprint[ 0 ].z );
 
-  EXPECT_NEAR( -1.35355f, footprint[ 1 ].x, 0.0001f);
-  EXPECT_NEAR( 1.35355f, footprint[ 1 ].y, 0.0001f);
-  EXPECT_EQ( 0.0f, footprint[ 1 ].z);
+  EXPECT_EQ( -1.5f, footprint[ 1 ].x );
+  EXPECT_EQ( 1.5f, footprint[ 1 ].y );
+  EXPECT_EQ( 0.0f, footprint[ 1 ].z );
 
-  EXPECT_NEAR( -1.22361f, footprint[ 2 ].x, 0.0001f);
-  EXPECT_NEAR( -1.44721f, footprint[ 2 ].y, 0.0001f);
-  EXPECT_EQ( 0.0f, footprint[ 2 ].z);
+  EXPECT_EQ( -1.5f, footprint[ 2 ].x );
+  EXPECT_EQ( -1.5f, footprint[ 2 ].y );
+  EXPECT_EQ( 0.0f, footprint[ 2 ].z );
 }
 
 TEST( Costmap2DROS, radius_param )
@@ -155,7 +155,7 @@ TEST( Costmap2DROS, footprint_empty )
   // With no specification of footprint or radius, defaults to 0.46 meter radius plus 0.01 meter padding.
   EXPECT_EQ( 16, footprint.size() );
 
-  EXPECT_NEAR( 0.47f, footprint[ 0 ].x, 0.001 );
+  EXPECT_NEAR( 0.47f, footprint[ 0 ].x, 0.0001 );
   EXPECT_NEAR( 0.0f, footprint[ 0 ].y, 0.0001 );
   EXPECT_EQ( 0.0f, footprint[ 0 ].z );
 }
